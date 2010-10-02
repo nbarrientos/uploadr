ca6be77e6d0f714c1dc7a006150285794582e212
Nacho Barrientos <nacho@criptonita.com>
2010-10-02 13:16:47 +0200

    Count references instead of IN_FS files

--- handlers/main.py
+++ handlers/main.py
@@ -2,6 +2,7 @@ import tornado.options
 import tornado.web
 
 from handlers.base import BaseHandler
+from status import UPLOAD_STATUS as us
 
 class MainHandler(BaseHandler):
     def get(self):
@@ -12,7 +13,9 @@ class MainHandler(BaseHandler):
         raise tornado.web.HTTPError(403)
 
     def _count_hosted_files(self):
-        row = self.db.get("SELECT COUNT(*) as c FROM files")
+        row = self.db.get("SELECT COUNT(*) as c \
+                        FROM uploads WHERE \
+                        uploads.status = %u" % us['ONLINE'])
         if row is not None: 
             row = row['c']
         return row
