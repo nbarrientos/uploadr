e1ec6d8eb1d399c1e481cd877158e46bcdf58733
Nacho Barrientos <nacho@criptonita.com>
2010-10-02 12:31:07 +0200

    Token validation on download requests.
    Transition from IN_USE to REDEEMED is useless since it does not take
    into account whether the download has finished or not. This should be
    fixed somehow.
    
    Idea: Async request.

--- handlers/download.py
+++ handlers/download.py
@@ -13,6 +13,7 @@ from tornado.options import options
 from handlers.base import BaseHandler
 from status import UPLOAD_STATUS as us
 from status import FILE_STATUS as fs
+from status import TOKEN_STATUS as ts
 import tools
 
 class RequestHandler(BaseHandler):
@@ -60,11 +61,19 @@ class RequestHandler(BaseHandler):
 class DownloadHandler(BaseHandler):
     
     def get(self, reference, filename):
-        # Todo: Validate self.get_argument("token")
+        # Fixme? if no default and argument not found fw raises 404 :/
+        token = self.get_argument("token", default=None)
+        if token is None:
+            raise tornado.web.HTTPError(400)
 
         logging.debug("Downloading file %s as %s" % (reference, filename))
         if not tools.validate_reference(reference):
             raise tornado.web.HTTPError(404)
+
+        if not self._validate_token(token):
+            logging.debug("Token %s is no longer valid to get %s" %
+                        (token, reference))
+            raise tornado.web.HTTPError(403)
         
         filesize, local_fileid, content_type = self._lookup_upload(reference)
 
@@ -74,6 +83,8 @@ class DownloadHandler(BaseHandler):
         path = "/".join([options.storage, local_fileid])
         logging.debug("About to deliver : %s, Size: %s C-T: %s" % 
                 (path, filesize, content_type))
+
+        self._set_token_in_use(token)
 
         self.set_header("Content-Length", filesize)
         self.set_header("Content-Type", content_type)
@@ -85,6 +96,9 @@ class DownloadHandler(BaseHandler):
             self.write(buf)
             buf = in_file.read(100)
         in_file.close()
+
+        self.flush()
+        self._set_token_redeemed(token)
 
     def post(self):
         raise tornado.web.HTTPError(403)
@@ -105,3 +119,33 @@ class DownloadHandler(BaseHandler):
             content_type = row['c_t']
         
         return (filesize, local_fileid, content_type)
+
+    def _validate_token(self, token):
+        logging.debug("Validating download token %s" % token)
+        client_ip = self.request.remote_ip
+        row = self.db.get("SELECT tokens.token FROM tokens \
+                WHERE tokens.token = '%s' \
+                AND tokens.ip = '%s' \
+                AND tokens.expires > NOW() \
+                AND tokens.status = %u" % 
+                (token, client_ip, ts['PENDING']))
+
+        return row is not None
+
+    def _set_token_in_use(self, token):
+        logging.debug("Setting IN_USE status to download token %s" % token)
+        row = self.db.execute("UPDATE tokens SET status=%u \
+                WHERE token = '%s'" %
+                (ts['IN_USE'], token))
+
+        if row is None:
+            raise RuntimeError("Token status update failed (IN_USE)")
+
+    def _set_token_redeemed(self, token):
+        logging.debug("Setting REDEEMED status to download token %s" % token)
+        row = self.db.execute("UPDATE tokens SET status=%u \
+                WHERE token = '%s'" %
+                (ts['REDEEMED'], token))
+
+        if row is None:
+            raise RuntimeError("Token status update failed (REDEEMED)")
